7f71ca32c9b68d2725887d967d25615891921ac4
vagner ventura <vagnerventura@hotmail.com>
2024-08-08 21:27:28 -0300

    Criando o Projeto

---
+++ src/main/java/com/Vagner/Sistema/Startup.java
@@ -0,0 +1,13 @@
+package com.Vagner.Sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
